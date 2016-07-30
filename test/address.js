07d2bf48c8884e9bc3a54c6a9d95cb6987760336
GwonHyeok <ghyeok@daycore.co.kr>
2016-07-30 22:38:09 +0900

    GeoLocation Lib
    getRatedAddress: 받아온 주소 데이터를 가지고 가장 가까운 위치의 주소 정보를 가져온다

--- test/address.js
+++ test/address.js
@@ -6,13 +6,22 @@ const GeoLocation = require('../lib/geolocation');
 
 describe('GeoLocation', function () {
     describe('좌표로 주소 정보를 받아옴', function () {
+        const location = new GeoLocation('37.49543016888596', '127.03781811461468');
+        let address;
 
         it('오류 없이 좌표 정보로 주소를 받아와야 한다', function (done) {
-            const location = new GeoLocation('37.49543016888596', '127.03781811461468');
             location.getAddress(function (error, result) {
+                address = result;
+
                 done(error);
             });
-        })
+        });
 
-    })
+        it('받아온 주소 데이터를 가지고 가장 가까운 위치의 주소 정보를 가져온다', (done) => {
+            location.getRatedAddress(address)
+                .then((data) => {
+                    done();
+                })
+        });
+    });
 });
