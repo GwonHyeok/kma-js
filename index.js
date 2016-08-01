100e28c86881bc20f727e636d7f3f5aefaaa5fb3
GwonHyeok <ghyeok@daycore.co.kr>
2016-08-01 19:51:53 +0900

    index에 Weather, GeoLocation, KMA 라이브러리 exports

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by GwonHyeok on 2016. 8. 1..
+ */
+
+exports.Weather = require('./lib/weather');
+exports.GeoLocation = require('./lib/geolocation');
+exports.Kma = require('./lib/kma');
